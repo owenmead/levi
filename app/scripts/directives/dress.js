3fa2484e93eb654fbebd78cbb57c356be3843cac
owenmead <owenmead@gmail.com>
2015-06-06 13:31:36 -0700

    Bit of code cleanup

--- app/scripts/directives/dress.js
+++ app/scripts/directives/dress.js
@@ -4,9 +4,6 @@ angular.module('leviApp')
   .directive('dress', function () {
     return {
       template: '<div><img src="images/dress.png" /></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        // Linky linky
-      }
+      restrict: 'E'
     };
   });
--- app/scripts/directives/pieceSelector.js
+++ app/scripts/directives/pieceSelector.js
@@ -6,23 +6,22 @@ angular.module('leviApp')
       templateUrl: 'scripts/directives/pieceSelector.html',
       restrict: 'E',
       scope: {},
-      link: function postLink(scope, element, attrs) {
-
-        scope.showSwatchs = false;
-
-        scope.openSwatch = function() {
-            scope.showSwatchs = true;
+      controller: function($scope, $attrs) {
+        console.log($attrs);
+        $scope.showSwatchs = false;
+        $scope.openSwatch = function() {
+            $scope.showSwatchs = true;
         };
-        scope.closeSwatchs = function() {
-            scope.showSwatchs = false;
+        $scope.closeSwatchs = function() {
+            $scope.showSwatchs = false;
         };
-        scope.selectSwatch = function(swatch) {
+        $scope.selectSwatch = function(swatch) {
             if (swatch === 'blue') {
                 swatch = 'none';
             }
-            scope.pieceColour = swatch;
+            $scope.pieceColour = swatch;
         };
-        scope.selectSwatch('blue');
+        $scope.selectSwatch('blue');
       }
     };
   });
