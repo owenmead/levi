e5add4202eeb97ee547828425871a42626697ef2
owenmead <owenmead@gmail.com>
2015-06-07 21:00:42 -0700

    Use the image for the swatch

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,11 @@ angular.module('leviApp')
         name: 'shoulder',
         imgDir: 'images/shoulder/',
         options: {
-          green:  {img:'green.png',  swatchStyle: 'background-color: #0F0;'},
-          blue:   {img:'none.png',   swatchStyle: 'background-color: #00F;'},
-          purple: {img:'purple.png', swatchStyle: 'background-color: #F0F;'},
-          red:    {img:'red.png',    swatchStyle: 'background-color: #F00;'},
-          yellow: {img:'yellow.png', swatchStyle: 'background-color: #FF0;'}
+          green:  {img:'green.png',  swatchStyle: 'background: url(images/shoulder/green.png) -43px -16px no-repeat;'},
+          blue:   {img:'none.png',   swatchStyle: 'background: url(images/shoulder/blue.png) -43px -16px no-repeat;'},
+          purple: {img:'purple.png', swatchStyle: 'background: url(images/shoulder/purple.png) -43px -16px no-repeat;'},
+          red:    {img:'red.png',    swatchStyle: 'background: url(images/shoulder/red.png) -43px -16px no-repeat;'},
+          yellow: {img:'yellow.png', swatchStyle: 'background: url(images/shoulder/yellow.png) -43px -16px no-repeat;'}
         },
         positioning: {
           swatches: {left: '350px', top: '52px' },
@@ -25,9 +25,9 @@ angular.module('leviApp')
         name: 'trim',
         imgDir: 'images/trim/',
         options: {
-          blue: {img:'blue.png', swatchStyle: 'background-color: #00F;'},
-          fir:  {img:'fir.png',  swatchStyle: 'background-color: #F0F;'},
-          grey: {img:'grey.png', swatchStyle: 'background-color: #F00;'}
+          blue: {img:'blue.png', swatchStyle: 'background: url(images/trim/blue.png) -13px -74px no-repeat;'},
+          fir:  {img:'fir.png',  swatchStyle: 'background: url(images/trim/fir.png) -13px -74px no-repeat;'},
+          grey: {img:'grey.png', swatchStyle: 'background: url(images/trim/grey.png) -13px -74px no-repeat;'}
         },
         positioning: {
           swatches: {left: '32px', top: '115px' },
