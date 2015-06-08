5e56f9f59bc97745838f3fa0bbcb3aea18d90e41
owenmead <owenmead@gmail.com>
2015-06-07 20:07:38 -0700

    Change style to swatchStyle to be more explicit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,11 @@ angular.module('leviApp')
         name: 'shoulder',
         imgDir: 'images/shoulder/',
         options: {
-          green:  {img:'green.png',  style: 'background-color: #0F0;'},
-          blue:   {img:'none.png',   style: 'background-color: #00F;'},
-          purple: {img:'purple.png', style: 'background-color: #F0F;'},
-          red:    {img:'red.png',    style: 'background-color: #F00;'},
-          yellow: {img:'yellow.png', style: 'background-color: #FF0;'}
+          green:  {img:'green.png',  swatchStyle: 'background-color: #0F0;'},
+          blue:   {img:'none.png',   swatchStyle: 'background-color: #00F;'},
+          purple: {img:'purple.png', swatchStyle: 'background-color: #F0F;'},
+          red:    {img:'red.png',    swatchStyle: 'background-color: #F00;'},
+          yellow: {img:'yellow.png', swatchStyle: 'background-color: #FF0;'}
         }
       }
     }
