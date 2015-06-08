9ca352c9febadcb392ae6b279eae5e641e3c27de
owenmead <owenmead@gmail.com>
2015-06-07 20:48:33 -0700

    Can now select top trim. Works!

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,7 +18,29 @@ angular.module('leviApp')
           swatchOpener: { top: '50px', left: '215px', height: '200px', width: '133px' },
           pieceImg: { top: '53px', left: '207px', width: '144px', height: '276px' }
         }
+      },
+
+
+      trim: {
+        name: 'trim',
+        imgDir: 'images/trim/',
+        options: {
+          green:  {img:'align.png', swatchStyle: 'background-color: #0F0;'},
+          blue:   {img:'blue.png',  swatchStyle: 'background-color: #00F;'},
+          purple: {img:'fir.png',   swatchStyle: 'background-color: #F0F;'},
+          red:    {img:'grey.png',  swatchStyle: 'background-color: #F00;'}
+        },
+        positioning: {
+          swatches: {left: '32px', top: '115px' },
+          swatchOpener: { top: '77px', left: '65px', height: '124px', width: '145px' },
+          pieceImg: { top: '74px', left: '71px', width: '152px', height: '131px' }
+        }
       }
+
+
+
+
+
     };
     $scope.data = data;
   });
